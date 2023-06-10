18480cf27574b3bf43247fb2bf4d81cd479bac22
Maria Komendantova <masha.komendantova@yandex.ru>
2023-06-10 14:45:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue theQueue = new Queue(5);
+        theQueue.insert(10);
+        theQueue.insert(20);
+        theQueue.insert(30);
+        theQueue.insert(40);
+
+        theQueue.remove();
+        theQueue.remove();
+        theQueue.remove();
+
+        theQueue.insert(50);
+        theQueue.insert(60);
+        theQueue.insert(70);
+        theQueue.insert(80);
+
+        while (!theQueue.isEmpty()) {
+            System.out.print(theQueue.remove() + " ");
+        }
+    }
+}
